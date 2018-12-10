a0b1a4a4341dab8a42d2ea8b0b22588297b09f33
Kylie Powers <kyliepowers1@mail.weber.edu>
2018-12-10 01:15:36 -0700

    .

--- newMultiuserProjectTracker/newMultiuserProjectTracker/Hubs/MultiUserHub.cs
+++ newMultiuserProjectTracker/newMultiuserProjectTracker/Hubs/MultiUserHub.cs
@@ -6,23 +6,12 @@ namespace newMultiuserProjectTracker.Hubs
 {
     public class MultiUserHub : Hub
     {
-        private static int bearTally = 0, beetsTally = 0, battlestarTally = 0;
-
-        public async Task SendMessage(string user, string message)
+        public async Task CreateGroup(string name)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
-        }
+            //TODO: put data in database
 
-        public async Task SendResults(bool bears, bool beets, bool battlestar)
-        {
-            if (bears)
-                bearTally++;
-            if (beets)
-                beetsTally++;
-            if (battlestar)
-                battlestarTally++;
-
-            await Clients.All.SendAsync("ReceiveResults", bearTally, beetsTally, battlestarTally);
+            //await Clients.Caller.SendAsync("NewGroupPage", name);
+            await Clients.Caller.SendAsync("NewGroupPage", name);
         }
 
     }
--- newMultiuserProjectTracker/newMultiuserProjectTracker/wwwroot/js/MultiUser.js
+++ newMultiuserProjectTracker/newMultiuserProjectTracker/wwwroot/js/MultiUser.js
@@ -1,75 +1,117 @@
 ﻿//"use strict";
 
 var connection = new signalR.HubConnectionBuilder().withUrl("/MultiUserHub").build();
-var Bears, Beets, Battlestar;
-var chart;
-
-
-connection.on("ReceiveMessage", function (user, message) {
-    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-    var encodedMsg = user + " says " + msg;
-    var li = document.createElement("li");
-    li.textContent = encodedMsg;
-    document.getElementById("messagesList").appendChild(li);
-
-});
-
-
-var li = document.createElement("li");
-connection.on("ReceiveResults", function (bears, beets, battlestar) {
-    //var li = document.createElement("li");
-    //    var encodedMsg = "Bears: " + bears + "\n" + "Beets: " + beets + "\n" + "Battlestar Gallactica: " + battlestar;
-    //    li.textContent = encodedMsg;
-    //document.getElementById("resultsList").appendChild(li);
-
-    chart = new CanvasJS.Chart("chartContainer",
-        {
-            theme: "dark2", // "light1", "light2", "dark1", "dark2"
-            exportEnabled: true,
-            animationEnabled: true,
-            title: {
-                text: "Survey"
-            },
-            data: [
-                {
-                    type: "column",
-                    startAngle: 25,
-                    toolTipContent: "<b>{label}</b>: {y}%",
-                    showInLegend: "true",
-                    legendText: "{label}",
-                    indexLabelFontSize: 16,
-                    indexLabel: "{label} - {y}",
-                    dataPoints: [
-                        { y: bears, label: "Bears" },
-                        { y: beets, label: "Beets" },
-                        { y: battlestar, label: "Battlestar Gallactica" },
-                    ]
-                }
-            ]
-        });
-    chart.render();
-});
-
+connection.onclose(() => setTimeout(startSignalRConnection(connection), 5000));
 
 connection.start().catch(function (err) {
     return console.error(err.toString());
 });
 
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById("indexChooseGroup").addEventListener("click", showProject, false);
 
-document.getElementById("submitButton").addEventListener("click", function (event) {
+});
 
-    var bears = document.getElementById("bears").checked;
-    var beets = document.getElementById("beets").checked;
-    var battlestar = document.getElementById("battlestar").checked;
 
-    if (!bears && !beets && !battlestar) {
-        alert('Attention: Please make a selection!');
+function showProject() {
+    var x = document.getElementById("projects");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    }
+    else {
+        x.style.display = "none";
+    }
+}
+
+//CREATE GROUP
+document.getElementById("createGroupSubmit").on("click", ShowNewGroupElements, false);
+function ShowNewGroupElements() {
+
+    //Hide Other Elements
+    document.getElementById("chooseGroupDiv").style.display = "none";
+    document.getElementById("projects").style.display = "none";
+    document.getElementById("lblchoosGrp").style.display = "none";
+    document.getElementById("orBreak").style.display = "none";
+
+    //Display Needed Elements
+    document.getElementById("newGroupNameDiv").style.display = "block";
+    document.getElementById("createGroupSubmitDiv").style.display = "none";
+
+};
+
+
+document.getElementById("submitNewGroup").on("click", ShowNewGroupBoard, false);
+function ShowNewGroupBoard() {
+
+    var groupName = document.getElementById("newGroupName").value;
+    if (groupName === "") {
+        alert('Attention: Please enter a group name!');
+        return;
+    } else {
+        connection.invoke("CreateGroup", groupName).catch(function (err) {
+            return console.error(err.toString());
+        });
+
+        document.getElementById("newGroupNameDiv").style.display = "none";
+        document.getElementById("newGroupCreatedDiv").style.display = "block";
+        document.getElementById("lblGroupName").innerText = groupName;
+
+        showNewProject(); 
     }
 
-    connection.invoke("SendResults", bears, beets, battlestar).catch(function (err) {
-        return console.error(err.toString());
-    });
     event.preventDefault();
-});
+};
+
+function showNewProject() {
+    //TODO: Do we want a number of new elements?
+    var x = document.getElementById("projects");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    }
+    else {
+        x.style.display = "none";
+    }
+}
+
+function newWorkItem() {
+
+    var br = document.createElement("BR");
+
+    var clone = document.createElement("textarea");
+
+    var lock = document.createElement("button");
+    var lockt = document.createTextNode("Lock");       // Create a text node
+    lock.appendChild(lockt);
+    //document.body.appendChild(lock);
+    //clone.appendChild(lock);
+
+    var unlock = document.createElement("button");
+    var unlockt = document.createTextNode("Unlock");
+    unlock.appendChild(unlockt);
+    //document.body.appendChild(unlock);
+    //clone.appendChild(unlock);
+
+    document.getElementById("table").appendChild(clone);
+
+    document.getElementById("table").appendChild(lock); 
+    document.getElementById("table").appendChild(unlock); 
+
+    //var row = document.getElementById("projects"); 
+    var table = document.getElementById("table"); 
+    //var clone = row.cloneNode(true); 
+    //clone.id = "newID"; 
+
+    table.appendChild(clone); 
+
+    /*
+     *
+     *     var row = document.getElementById("rowToClone"); // find row to copy
+     var table = document.getElementById("tracklist"); // find table to append to
+     var clone = row.cloneNode(true); // copy children too
+     clone.id = "newID"; // change id or other attributes/contents
+     table.appendChild(clone); // add new row to end of table
+     */
+
+}
 
 

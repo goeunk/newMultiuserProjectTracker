2b080fe71769a8072ae412a0b5a8ff8d42bdd012
Kylie Powers <kyliepowers1@mail.weber.edu>
2018-12-12 22:03:55 -0700

    latest changes

--- newMultiuserProjectTracker/newMultiuserProjectTracker/Hubs/MultiUserHub.cs
+++ newMultiuserProjectTracker/newMultiuserProjectTracker/Hubs/MultiUserHub.cs
@@ -6,13 +6,66 @@ namespace newMultiuserProjectTracker.Hubs
 {
     public class MultiUserHub : Hub
     {
-        public async Task CreateGroup(string name)
-        {
-            //TODO: put data in database
+        //public async Task CreateGroup(string name)
+        //{
+        //    //TODO: put data in database
 
-            //await Clients.Caller.SendAsync("NewGroupPage", name);
-            await Clients.Caller.SendAsync("NewGroupPage", name);
+        //    //await Clients.Caller.SendAsync("NewGroupPage", name);
+        //    await Clients.Caller.SendAsync("NewGroupPage", name);
+        //}
+
+        //public async Task GetProjectWithGroupName(string groupName)
+        //{
+        //    //TODO: Retrieve group from database
+
+        //    await Clients.Caller.SendAsync("");
+        //}
+
+        //public async Task SendMessage(string user, string message)
+        //{
+        //    await Clients.All.SendAsync("ReceiveMessage", user, message);
+        //}
+
+        //public async Task UpdateTextBox(string textboxID, string message)
+        //{
+
+        //    await Clients.Others.SendAsync("UpdateText", textboxID, message);
+        //}
+
+        //public async Task LockText(string textboxID)
+        //{
+        //    await Clients.Others.SendAsync("LockTextBox", textboxID);
+        //}
+
+        //public async Task UnlockText(string textboxID)
+        //{
+        //    await Clients.Others.SendAsync("UnlockText", textboxID);
+        //}
+
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
 
+        public async Task UpdateTextBox(string textboxID, string message)
+        {
+
+            await Clients.Others.SendAsync("UpdateText", textboxID, message);
+        }
+
+        public async Task LockText(string textboxID)
+        {
+            await Clients.Others.SendAsync("LockTextBox", textboxID);
+        }
+
+        public async Task UnlockText(string textboxID)
+        {
+            await Clients.Others.SendAsync("UnlockText", textboxID);
+        }
+
+        public async Task CreateNewTextBox(string textboxID)
+        {
+            await Clients.All.SendAsync("CreateNewTextBox", textboxID);
+        }
     }
 }
--- newMultiuserProjectTracker/newMultiuserProjectTracker/wwwroot/js/MultiUser.js
+++ newMultiuserProjectTracker/newMultiuserProjectTracker/wwwroot/js/MultiUser.js
@@ -1,117 +1,237 @@
-﻿//"use strict";
+﻿"use strict";
 
 var connection = new signalR.HubConnectionBuilder().withUrl("/MultiUserHub").build();
-connection.onclose(() => setTimeout(startSignalRConnection(connection), 5000));
 
 connection.start().catch(function (err) {
     return console.error(err.toString());
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById("indexChooseGroup").addEventListener("click", showProject, false);
 
+connection.on("UpdateText", function (textboxId, message) {
+    console.log("update text: " + message);
+    document.getElementById(textboxId).value = message;
 });
 
 
-function showProject() {
-    var x = document.getElementById("projects");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    }
-    else {
-        x.style.display = "none";
-    }
-}
+connection.on("LockTextBox", function (textBoxId) {
+    console.log("text box is locked : " + textBoxId);
+    document.getElementById(textBoxId).disabled = true;
+});
 
-//CREATE GROUP
-document.getElementById("createGroupSubmit").on("click", ShowNewGroupElements, false);
-function ShowNewGroupElements() {
+connection.on("UnlockText", function (textBoxId) {
+    console.log("text box is unlocked : " + textBoxId);
+    document.getElementById(textBoxId).disabled = false;
+});
 
-    //Hide Other Elements
-    document.getElementById("chooseGroupDiv").style.display = "none";
-    document.getElementById("projects").style.display = "none";
-    document.getElementById("lblchoosGrp").style.display = "none";
-    document.getElementById("orBreak").style.display = "none";
+connection.on("CreateNewTextBox", function (textBoxId) {
+    console.log("create new text box : " + textBoxId);
+    CreateNewWorkObject(textBoxId);
+});
 
-    //Display Needed Elements
-    document.getElementById("newGroupNameDiv").style.display = "block";
-    document.getElementById("createGroupSubmitDiv").style.display = "none";
-
+var onKeyupFunction = function (event) {
+    var element = this.id;
+    var text = this.value;
+    //console.log(event);
+    //console.log("element id : " + element)
+    connection.invoke("UpdateTextBox", element, text).catch(function (err) {
+        return console.error(err.toString);
+    });
 };
 
-
-document.getElementById("submitNewGroup").on("click", ShowNewGroupBoard, false);
-function ShowNewGroupBoard() {
-
-    var groupName = document.getElementById("newGroupName").value;
-    if (groupName === "") {
-        alert('Attention: Please enter a group name!');
-        return;
-    } else {
-        connection.invoke("CreateGroup", groupName).catch(function (err) {
-            return console.error(err.toString());
-        });
-
-        document.getElementById("newGroupNameDiv").style.display = "none";
-        document.getElementById("newGroupCreatedDiv").style.display = "block";
-        document.getElementById("lblGroupName").innerText = groupName;
-
-        showNewProject(); 
-    }
-
-    event.preventDefault();
+var onFocus = function (event) {
+    var elementId = this.id;
+    connection.invoke("LockText", elementId).catch(function (err) {
+        return console.error(err.toString);
+    });
 };
 
-function showNewProject() {
-    //TODO: Do we want a number of new elements?
-    var x = document.getElementById("projects");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    }
-    else {
-        x.style.display = "none";
-    }
+var onBlur = function (event) {
+    var elementId = this.id;
+    connection.invoke("UnlockText", elementId).catch(function (err) {
+        return console.error(err.toString);
+    });
 }
 
-function newWorkItem() {
+//document.getElementById("messagesList").addEventListener("keyup", onKeyupFunction);
+//document.getElementById("messagesList2").addEventListener("keyup", onKeyupFunction);
+//document.getElementById("messagesList").addEventListener("focus", onFocus);
+//document.getElementById("messagesList2").addEventListener("focus", onFocus);
+//document.getElementById("messagesList").addEventListener("blur", onBlur);
+//document.getElementById("messagesList2").addEventListener("blur", onBlur);
 
-    var br = document.createElement("BR");
 
-    var clone = document.createElement("textarea");
+function ChooseGroup() {
 
-    var lock = document.createElement("button");
-    var lockt = document.createTextNode("Lock");       // Create a text node
-    lock.appendChild(lockt);
-    //document.body.appendChild(lock);
-    //clone.appendChild(lock);
+    //document.getElementById("messagesList").style.display = "block";
+    //CreateNewWorkObject();
+    var elementid = "messagesList" + countMyself();
+    connection.invoke("CreateNewTextBox", elementid).catch(function (err) {
+        return console.error(err.toString);
+    });
+}
 
-    var unlock = document.createElement("button");
-    var unlockt = document.createTextNode("Unlock");
-    unlock.appendChild(unlockt);
-    //document.body.appendChild(unlock);
-    //clone.appendChild(unlock);
+function CreateNewWorkObject(textid) {
+    var textarea = document.createElement("TEXTAREA");
+    textarea.addEventListener("blur", onBlur);
+    textarea.addEventListener("focus", onFocus);
+    textarea.addEventListener("keyup", onKeyupFunction);
 
-    document.getElementById("table").appendChild(clone);
+    textarea.setAttribute("id", textid);
+    document.body.appendChild(textarea);
+}
 
-    document.getElementById("table").appendChild(lock); 
-    document.getElementById("table").appendChild(unlock); 
+function countMyself() {
 
-    //var row = document.getElementById("projects"); 
-    var table = document.getElementById("table"); 
-    //var clone = row.cloneNode(true); 
-    //clone.id = "newID"; 
+    connection.invoke("Increment", elementid).catch(function (err) {
+        return console.error(err.toString);
 
-    table.appendChild(clone); 
+    // Check to see if the counter has been initialized
+    if (typeof countMyself.counter == 'undefined') {
+        // It has not... perform the initialization
+        countMyself.counter = 0;
+    }
 
-    /*
-     *
-     *     var row = document.getElementById("rowToClone"); // find row to copy
-     var table = document.getElementById("tracklist"); // find table to append to
-     var clone = row.cloneNode(true); // copy children too
-     clone.id = "newID"; // change id or other attributes/contents
-     table.appendChild(clone); // add new row to end of table
-     */
+    // Do something stupid to indicate the value
+    console.log(++countMyself.counter);
+    return countMyself.counter;
 
 }
 
 
+
+
+////"use strict";
+
+//var connection = new signalR.HubConnectionBuilder().withUrl("/MultiUserHub").build();
+//connection.onclose(() => setTimeout(startSignalRConnection(connection), 5000));
+
+////document.onload(() => getElementById("messagesList").style.display = hidden);
+
+
+//connection.start().catch(function (err) {
+//    return console.error(err.toString());
+//});
+
+//document.addEventListener("DOMContentLoaded", function () {
+//    document.getElementById("indexChooseGroup").addEventListener("click", showProject, false);
+   
+//});
+
+//document.getElementById("messagesList").addEventListener("keyup", onKeyupFunction);
+//document.getElementById("messagesList").addEventListener("focus", onFocus);
+//document.getElementById("messagesList").addEventListener("blur", onBlur);
+
+//function showProject() {
+
+//    var groupSelect = document.getElementById("selectChooseGroup").text;
+//    var groupName = groupSelect.options[groupSelect.selectedIndex].text;
+
+
+//    connection.invoke("GetProjectWithGroupName", groupName).catch(function(err) {
+//        return console.error(err.toString());
+//    });
+
+//    var x = document.getElementById("messagesList");
+//    if (x.style.display === "none") {
+//        x.style.display = "block";
+//    }
+//    else {
+//        x.style.display = "none";
+//    }
+//}
+
+////CREATE GROUP
+//var nl = document.getElementById("createGroupSubmit");
+//if (nl) {
+//    nl.addEventListener("click", ShowNewGroupElements, false);
+//}
+
+//function ShowNewGroupElements() {
+
+//    //Hide Other Elements
+//    document.getElementById("chooseGroupDiv").style.display = "none";
+//    document.getElementById("messagesList").style.display = "none";
+//    document.getElementById("lblchoosGrp").style.display = "none";
+//    document.getElementById("orBreak").style.display = "none";
+
+//    //Display Needed Elements
+//    document.getElementById("newGroupNameDiv").style.display = "block";
+//    document.getElementById("createGroupSubmitDiv").style.display = "none";
+
+//};
+
+//var nl = document.getElementById("submitNewGroup");
+//if (nl) {
+//    nl.addEventListener("click", ShowNewGroupBoard, false);
+//}
+//function ShowNewGroupBoard() {
+
+//    var groupName = document.getElementById("newGroupName").value;
+//    if (groupName === "") {
+//        alert('Attention: Please enter a group name!');
+//    } else {
+//        connection.invoke("CreateGroup", groupName).catch(function (err) {
+//            return console.error(err.toString());
+//        });
+
+//        document.getElementById("newGroupNameDiv").style.display = "none";
+//        document.getElementById("newGroupCreatedDiv").style.display = "block";
+//        document.getElementById("lblGroupName").innerText = groupName;
+
+//        showNewProject(); 
+//    }
+
+//    //event.preventDefault();
+//};
+
+//function showNewProject() {
+//    //TODO: Do we want a number of new elements?
+//    var x = document.getElementById("messagesList");
+//    if (x.style.display === "none") {
+//        x.style.display = "block";
+//    }
+//    else {
+//        x.style.display = "none";
+//    }
+//}
+
+////function newWorkItem() {
+
+////    var br = document.createElement("BR");
+
+////    var clone = document.createElement("textarea");
+
+////    var lock = document.createElement("button");
+////    var lockt = document.createTextNode("Lock");       // Create a text node
+////    lock.appendChild(lockt);
+////    //document.body.appendChild(lock);
+////    //clone.appendChild(lock);
+
+////    var unlock = document.createElement("button");
+////    var unlockt = document.createTextNode("Unlock");
+////    unlock.appendChild(unlockt);
+////    //document.body.appendChild(unlock);
+////    //clone.appendChild(unlock);
+
+////    document.getElementById("table").appendChild(clone);
+
+////    document.getElementById("table").appendChild(lock); 
+////    document.getElementById("table").appendChild(unlock); 
+
+////    //var row = document.getElementById("messagesList");
+////    var table = document.getElementById("table"); 
+////    //var clone = row.cloneNode(true); 
+////    //clone.id = "newID"; 
+
+////    table.appendChild(clone); 
+
+////    /*
+////     *
+////     *     var row = document.getElementById("rowToClone"); // find row to copy
+////     var table = document.getElementById("tracklist"); // find table to append to
+////     var clone = row.cloneNode(true); // copy children too
+////     clone.id = "newID"; // change id or other attributes/contents
+////     table.appendChild(clone); // add new row to end of table
+////     */
+
+////}
